bcc2a379a719e267b7d130b881e2061ba9838453
Blair Currey <curreyb88@gmail.com>
2020-09-28 18:05:52 -0400

    updated INSERT to reflect changes to schema (added session field)

--- db/add_dummy_reviews.sql
+++ db/add_dummy_reviews.sql
@@ -1,7 +1,7 @@
 INSERT INTO 
-	reviews(course_id, difficulty, workload, rating)
+	reviews(course_id, session, difficulty, workload, rating, text)
 VALUES
-	('CM1005', 2, 3, 4),
-	('CM1010', 3, 4, 3),
-    ('CM1015', 4, 4, 2),
-	('CM1005', 3, 4, 3);
+	('CM1005', 'October to March 2020', 2, 3, 4, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
+	('CM1010', 'April to September 2020', 3, 4, 3, 'Est ante in nibh mauris cursus mattis.'),
+    ('CM1015', 'October to March 2020'4, 4, 2, 'Commodo elit at imperdiet dui.'),
+	('CM1005', 'April to September 2020', 3, 4, 3, 'Mattis aliquam faucibus purus in massa tempor.');
