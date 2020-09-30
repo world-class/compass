61b45d73ca2668c588d89ef4e0543e5be9b4f673
Blair Currey <12960453+BlairCurrey@users.noreply.github.com>
2020-09-30 12:45:39 -0400

    added missing comma to line 6

--- db/add_dummy_reviews.sql
+++ db/add_dummy_reviews.sql
@@ -3,5 +3,5 @@ INSERT INTO
 VALUES
 	('CM1005', 'October to March 2020', 2, 3, 4, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
 	('CM1010', 'April to September 2020', 3, 4, 3, 'Est ante in nibh mauris cursus mattis.'),
-    ('CM1015', 'October to March 2020'4, 4, 2, 'Commodo elit at imperdiet dui.'),
+    ('CM1015', 'October to March 2020', 4, 4, 2, 'Commodo elit at imperdiet dui.'),
 	('CM1005', 'April to September 2020', 3, 4, 3, 'Mattis aliquam faucibus purus in massa tempor.');
