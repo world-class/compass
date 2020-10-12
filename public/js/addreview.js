7f2f3a91d7a04276c97f2903bf14608209de6e52
Blair Currey <curreyb88@gmail.com>
2020-10-12 16:27:15 -0400

    fixed sessions not loading on firefox

--- public/js/addreview.js
+++ public/js/addreview.js
@@ -1,6 +1,3 @@
 import { SessionOptions } from './modules/SessionOptions.js';
 
-//run everything dependent on jQuery in here
-jQuery(document).ready(function(){
-    new SessionOptions("session"); //make sessions
-});
+new SessionOptions("session"); //make sessions
--- public/js/editreview.js
+++ public/js/editreview.js
@@ -1,8 +1,5 @@
 import { SessionOptions } from './modules/SessionOptions.js';
 
-//run everything dependent on jQuery in here
-jQuery(document).ready(function(){
-    new SessionOptions("session"); //make sessions
-    const session = JSON.parse(reviewSession) //get session
-    $('#session').selectpicker('val', session); //set as selected
-});
+new SessionOptions("session"); //make sessions
+const session = JSON.parse(reviewSession) //get session
+$('#session').selectpicker('val', session); //set as selected
