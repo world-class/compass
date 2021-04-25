5179d2009f06810abe3b331618fe3cf3e8691491
Arjun Muralidharan <49580702+arjunmuralidharan@users.noreply.github.com>
2021-04-25 21:40:52 +0200

    Remove the old HTTPS certificate code
    Co-authored-by: Ankush Menat <ankushmenat@gmail.com>

--- controllers/HttpServer.js
+++ controllers/HttpServer.js
@@ -14,20 +14,6 @@ class HttpServer {
 			console.log(`HTTP Server started at port ${httpPort}`);
 		});
 
-		if (process.env.NODE_ENV === "PROD") {
-			const certDir = process.env.TLS_CERTDIR;
-			const tlsOptions = {
-				key: fs.readFileSync(certDir + "privkey.pem", "utf8"),
-				cert: fs.readFileSync(certDir + "cert.pem", "utf8"),
-				ca: fs.readFileSync(certDir + "chain.pem", "utf8"),
-			};
-
-			// create HTTPS server
-			const httpsServer = https.createServer(tlsOptions, app);
-
-			httpsServer.listen(httpsPort, () => {
-				console.log(`HTTPS Server started at port ${httpsPort}!`);
-			});
 		}
 	}
 }
